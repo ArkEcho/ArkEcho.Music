b43a8108955db7bb57e1e0ab617c487189d58342
Stephan Ziesenis <stephanz@gmx.de>
2021-07-05 20:35:47 +0200

    Korrektes Laden Statische Files; kopieren nach Output und UseWebRoot

--- ArkEcho_CSharp/ArkEcho.Server/Program.cs
+++ ArkEcho_CSharp/ArkEcho.Server/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.IO;
+using System.Reflection;
 
 namespace ArkEcho.Server
 {
@@ -11,11 +13,14 @@ namespace ArkEcho.Server
         {
             using (ArkEchoServer.Instance)
             {
+                // TODO: Server vorher instanzieren, host raus -> konfigurierbarer Port
                 // Start the WebHost, Server and Controllers
+                // Set WebRoot for Static Files (css etc.), use xcopy for output
                 using IWebHost host = WebHost.CreateDefaultBuilder(args)
                                 .UseUrls("https://*:5001")
                                 .UseKestrel()
                                 .UseStartup<Startup>()
+                                .UseWebRoot($"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\wwwroot")
                                 .Build();
 
                 if (host == null)
