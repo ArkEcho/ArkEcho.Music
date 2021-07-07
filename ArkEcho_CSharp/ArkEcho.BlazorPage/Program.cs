f3ccb256d1b79b290ff9b6ebf3ada598c1536666
Stephan Ziesenis <stephanz@gmx.de>
2021-07-07 21:18:23 +0200

    Umstellung BlazorPage lädt API Controller etc.

--- ArkEcho_CSharp/ArkEcho.BlazorPage/Program.cs
+++ ArkEcho_CSharp/ArkEcho.BlazorPage/Program.cs
@@ -1,17 +1,44 @@
+using ArkEcho.Server;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
 namespace ArkEcho.BlazorPage
 {
-    public class Program
+    public static class Program
     {
-        //public static void Main(string[] args)
-        //{
-        //    CreateHostBuilder(args).Build().Run();
-        //}
+        public static void Main(string[] args)
+        {
+            using (ArkEchoServer.Instance)
+            {
+                // TODO: Server vorher instanzieren, host raus -> konfigurierbarer Port
+                // Start the WebHost, Server and Controllers
+                // Set WebRoot for Static Files (css etc.), use xcopy for output
+                using IWebHost host = WebHost.CreateDefaultBuilder(args)
+                                .UseUrls("https://*:5001")
+                                .UseKestrel()
+                                .UseStartup<Startup>()
+                                //.UseWebRoot($"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\wwwroot")
+                                .Build();
 
-        //public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //    Host.CreateDefaultBuilder(args)
-        //        .ConfigureWebHostDefaults(webBuilder =>
-        //        {
-        //            webBuilder.UseStartup<Startup>();
-        //        });
+                if (host == null)
+                    return;
+
+                if (!ArkEchoServer.Instance.Init(host))
+                {
+                    Console.WriteLine("Problem on Initializing the ArkEcho.Server!");
+                    return;
+                }
+                else
+                {
+                    host.Run(); // Starts Event Cycle and API
+
+                    if (ArkEchoServer.Instance.RestartRequested)
+                        System.Diagnostics.Process.Start("ArkEcho.Server.exe");
+                }
+            }
+
+            Environment.Exit(0);
+        }
     }
 }
