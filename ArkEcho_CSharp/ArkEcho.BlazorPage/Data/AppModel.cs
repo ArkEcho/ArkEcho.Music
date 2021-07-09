648fdeda995ecc370353581709fc344f922c21f8
Stephan <stephanz@gmx.de>
2021-07-09 17:23:31 +0200

    NOtify bei Counter erhöhung

--- ArkEcho_CSharp/ArkEcho.BlazorPage/Data/AppModel.cs
+++ ArkEcho_CSharp/ArkEcho.BlazorPage/Data/AppModel.cs
@@ -7,8 +7,10 @@ namespace ArkEcho.BlazorPage
 {
     public class AppModel
     {
-        public int Counter { get; set; }
+        private int counter;
+        public int Counter { get { return counter; } set { counter = value; CounterChanged?.Invoke(); } }
 
+        public event Action CounterChanged;
         public AppModel()
         {
             Counter = 0;
