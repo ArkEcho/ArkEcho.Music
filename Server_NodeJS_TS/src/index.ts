3eddb2852fc76b281e7aa8452a4322c45e5f2946
Stephan <stephanz@gmx.de>
2021-11-20 14:33:16 +0100

    Repo aufräumen

--- Server_NodeJS_TS/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { WebsocketService }from './websocketservice';
-import { HttpServerService } from './httpserverservice';
-
-let wsservice : WebsocketService = new WebsocketService(8080);
-
-let httpservice : HttpServerService = new HttpServerService(3000);
