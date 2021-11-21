ad6a5863ec8b4adb666e66e19c03765eb3685275
Stephan <stephanz@gmx.de>
2021-11-21 17:46:53 +0100

    Exception Handling und Funktion für Dateipfad

--- ArkEcho.App/Source/Activity/SyncMusicFilesActivity.cs
+++ ArkEcho.App/Source/Activity/SyncMusicFilesActivity.cs
@@ -38,70 +38,108 @@ namespace ArkEcho.App
         private async void onSyncMusicFilesButtonClicked(object sender, EventArgs e)
         {
             AppModel.Instance.PreventLock();
-            logInListView("Loading Music Library from Remote Server", Core.Resources.LogLevel.Information);
 
-            string libraryString = await AppModel.Instance.Rest.GetMusicLibrary();
-            if (string.IsNullOrEmpty(libraryString))
+            MusicLibrary lib = null;
+            try
             {
-                logInListView("No response from the Server!", Core.Resources.LogLevel.Information);
-                AppModel.Instance.AllowLock();
-                return;
-            }
+                logInListView("Loading MusicLibrary", Core.Resources.LogLevel.Information);
 
-            if (!AppModel.Instance.SetMusicLibrary(libraryString))
-            {
-                logInListView("Cant load json!", Core.Resources.LogLevel.Information);
-                AppModel.Instance.AllowLock();
-                return;
-            }
-
-            MusicLibrary lib = AppModel.Instance.Library;
-            logInListView($"Music File Count: {lib.MusicFiles.Count.ToString()}", Core.Resources.LogLevel.Information);
-
-            string mediaFolderPath = AppModel.GetAndroidMediaAppSDFolderPath();
-            foreach (MusicFile file in lib.MusicFiles.FindAll(x => x.AlbumArtist == lib.AlbumArtists.Find(y => y.Name.Equals("Alligatoah")).GUID))
-            {
-                logInListView($"Loading {file.FileName}...", Core.Resources.LogLevel.Information);
-
-                file.Folder = $"{mediaFolderPath}/{lib.AlbumArtists.Find(x => x.GUID == file.AlbumArtist).Name}/{lib.Album.Find(x => x.GUID == file.Album).Name}";
-
-                if (!Directory.Exists(file.Folder))
-                    Directory.CreateDirectory(file.Folder);
-
-                if (File.Exists(file.GetFullFilePath()))
-                    continue;
-
-                byte[] fileBytes = await AppModel.Instance.Rest.GetMusicFile(file.GUID);
-
-                if (fileBytes.Length == 0)
+                string libraryString = await AppModel.Instance.Rest.GetMusicLibrary();
+                if (string.IsNullOrEmpty(libraryString))
                 {
-                    logInListView($"Error loading MusicFile {file.FileName} from Server!", Core.Resources.LogLevel.Information);
+                    logInListView("No response from the Server!", Core.Resources.LogLevel.Information);
+                    AppModel.Instance.AllowLock();
                     return;
                 }
-                else
-                    logInListView($"Writing {file.FileName}", Core.Resources.LogLevel.Information);
 
-                using (FileStream stream = new FileStream(file.GetFullFilePath(), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
+                if (!AppModel.Instance.SetMusicLibrary(libraryString))
                 {
-                    await stream.WriteAsync(fileBytes, 0, fileBytes.Length);
+                    logInListView("Cant load json!", Core.Resources.LogLevel.Information);
+                    AppModel.Instance.AllowLock();
+                    return;
                 }
+
+                lib = AppModel.Instance.Library;
+                logInListView($"Music File Count: {lib.MusicFiles.Count.ToString()}", Core.Resources.LogLevel.Information);
+
+                await Task.Delay(200);
+            }
+            catch (Exception ex)
+            {
+                logInListView($"Exception loading MusicLibrary: {ex.Message}", Core.Resources.LogLevel.Information);
+                AppModel.Instance.AllowLock();
+                return;
+            }
+
+            // TODO: Negativ Sync -> Ordner/dateien zu viel löschen
+
+            try
+            {
+                foreach (MusicFile file in lib.MusicFiles)
+                {
+                    file.Folder = getMusicFileFolder(file, lib);
+                    if (string.IsNullOrEmpty(file.Folder))
+                    {
+                        logInListView($"Error building Path for {file.Title}", Core.Resources.LogLevel.Information);
+                        AppModel.Instance.AllowLock();
+                        return;
+                    }
+
+                    if (!Directory.Exists(file.Folder))
+                        Directory.CreateDirectory(file.Folder);
+
+                    if (File.Exists(file.GetFullFilePath()))
+                        continue;
+
+                    logInListView($"Loading {file.Title}", Core.Resources.LogLevel.Information);
+
+                    byte[] fileBytes = await AppModel.Instance.Rest.GetMusicFile(file.GUID);
+
+                    if (fileBytes.Length == 0)
+                    {
+                        logInListView($"Error loading {file.Title} from Server!", Core.Resources.LogLevel.Information);
+                        AppModel.Instance.AllowLock();
+                        return;
+                    }
+
+                    using (FileStream stream = new FileStream(file.GetFullFilePath(), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
+                    {
+                        await stream.WriteAsync(fileBytes, 0, fileBytes.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logInListView($"Exception loading MusicFiles: {ex.Message}", Core.Resources.LogLevel.Information);
+                AppModel.Instance.AllowLock();
+                return;
             }
 
             logInListView($"Success!", Core.Resources.LogLevel.Information);
 
-            await Task.Delay(1000);
+            await Task.Delay(200);
 
             AppModel.Instance.AllowLock();
         }
 
-        private bool logInListView(string text, Resources.LogLevel level)
+        private string getMusicFileFolder(MusicFile file, MusicLibrary lib)
+        {
+            string mediaFolderPath = AppModel.GetAndroidMediaAppSDFolderPath();
+            Album album = lib.Album.Find(x => x.GUID == file.Album);
+            AlbumArtist artist = lib.AlbumArtists.Find(x => x.GUID == file.AlbumArtist);
+
+            if (album == null || artist == null)
+                return string.Empty;
+
+            return $"{mediaFolderPath}/{artist.Name}/{album.Name}";
+        }
+
+        private void logInListView(string text, Resources.LogLevel level)
         {
             adapter.Add($"{DateTime.Now:HH:mm:ss:fff}: {text}");
             adapter.NotifyDataSetChanged();
 
             logListView.SmoothScrollToPositionFromTop(logListView.LastVisiblePosition, 0);
-
-            return true;
         }
     }
 }
