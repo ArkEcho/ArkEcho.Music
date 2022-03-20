e55af0933ff4b537e842fc1ed658beac7d1ef40e
Stephan <stephanz@gmx.de>
2022-03-20 16:32:15 +0100

    Überarbeitung Server Log Writer, merken Dateiname, max Grösse etc.

--- ArkEcho.Server/Server/ServerLoggingWorker.cs
+++ ArkEcho.Server/Server/ServerLoggingWorker.cs
@@ -1,5 +1,6 @@
 ﻿using ArkEcho.Core;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -7,7 +8,24 @@ namespace ArkEcho.Server
 {
     public class ServerLoggingWorker : LoggingWorker
     {
+        private struct LogFile
+        {
+            public LogFile(string name, string fileName)
+            {
+                this.Name = name;
+                this.FileName = fileName;
+            }
+
+            public string Name { get; private set; }
+            public string FileName { get; private set; }
+        }
+
+        private string logFileExtension = ".log";
         private string logFolder = string.Empty;
+        private int maxFiles = 10;
+        private long logFileSizeMax = 10485760; // 10mb
+
+        private List<LogFile> logFiles = new List<LogFile>();
 
         public ServerLoggingWorker(string logFolder, Logging.LogLevel logLevel) : base(logLevel)
         {
@@ -16,22 +34,26 @@ namespace ArkEcho.Server
 
         protected override void HandleLogMessage(LogMessage log)
         {
-            using (StreamWriter fs = getFileStream(log))
-            {
-                fs?.WriteLine(log.ToLogString());
-                fs?.Flush();
-            }
-        }
-
-        private StreamWriter getFileStream(LogMessage logger)
-        {
-            // TODO: Log File Rotation, open new if Filestream voll
-
             StreamWriter fs = null;
+
+            LogFile file = logFiles.Find(x => x.Name == log.Name);
+            if (string.IsNullOrEmpty(file.FileName))
+            {
+                file = new LogFile(log.Name, getNewLogFileName(log)); // New Logging/File
+                logFiles.Add(file);
+            }
+            else
+            {
+                //FileInfo fileInfo = new FileInfo(fileName);
+                //if (fileInfo.Length > logFileSizeMax)
+                //{
+                //    List<string> files = logFiles.V;
+                //}
+            }
 
             try
             {
-                fs = new StreamWriter(new FileStream(getLogFileName(logger), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite), Encoding.UTF8);
+                fs = new StreamWriter(new FileStream($"{file.FileName}{logFileExtension}", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite), Encoding.UTF8);
                 fs.BaseStream.Seek(0, SeekOrigin.End);
             }
             catch (Exception ex)
@@ -39,12 +61,16 @@ namespace ArkEcho.Server
                 Console.WriteLine($"Exception opening Logging FileStream: {ex.Message}");
             }
 
-            return fs;
+            using (fs)
+            {
+                fs?.WriteLine(log.ToLogString());
+                fs?.Flush();
+            }
         }
 
-        private string getLogFileName(LogMessage logger)
+        private string getNewLogFileName(LogMessage logger)
         {
-            return Path.Combine(logFolder, $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}_{logger.Name}.log");
+            return Path.Combine(logFolder, $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}_{logger.Name}_0");
         }
 
         private bool disposed = false;
