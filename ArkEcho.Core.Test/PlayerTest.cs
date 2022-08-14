7d4d5efedafb2e6ef2a5eca50e72a797837820fd
Stephan Ziesenis <stephanz@gmx.de>
2022-08-14 09:49:39 +0200

    Unit Tests für Negativ Tests (Falsche Werte und Player nicht geladen) und Shuffle, ein song und mehrmals durch

--- ArkEcho.Core.Test/PlayerTest.cs
+++ ArkEcho.Core.Test/PlayerTest.cs
@@ -16,10 +16,47 @@ namespace ArkEcho.Core.Test
         private void StartPlayer(out Player testPlayer, out List<MusicFile> fileList)
         {
             CreatePlayer(out testPlayer);
+
             fileList = GetTestMusicLibrary().MusicFiles;
 
             bool started = testPlayer.Start(fileList, 0);
             Assert.IsTrue(started);
+        }
+
+        [TestMethod]
+        public void EmptyPlayerAndFalseValues()
+        {
+            Player player = new TimerTestPlayer();
+
+            player.Forward();
+            player.Backward();
+            player.Pause();
+            player.Play();
+            player.PlayPause();
+            player.Stop();
+
+            player.Volume = 5;
+            player.Volume = 500;
+            Assert.AreEqual(player.Volume, 5);
+
+            player.Volume = -20;
+            Assert.AreEqual(player.Volume, 5);
+
+            player.Shuffle = true;
+            player.Shuffle = false;
+
+            player.Position = 5;
+            player.Position = -20;
+            Assert.AreEqual(player.Position, 5);
+
+            player.Mute = true;
+            player.Mute = false;
+
+            bool started = player.Start(new List<MusicFile>(), -1);
+            Assert.IsFalse(started);
+
+            started = player.Start(null, 0);
+            Assert.IsFalse(started);
         }
 
         [TestMethod]
@@ -134,6 +171,58 @@ namespace ArkEcho.Core.Test
             Assert.IsFalse(testPlayer.Playing);
             Assert.IsTrue(testPlayer.Position == 0);
             Assert.IsTrue(testPlayer.PlayingFile.GUID == fileList[0].GUID);
+        }
+
+        [TestMethod]
+        public void ShuffleOneSong()
+        {
+            CreatePlayer(out Player testPlayer);
+            testPlayer.Shuffle = true;
+
+            bool started = testPlayer.Start(new List<MusicFile> { GetTestMusicLibrary().MusicFiles[0] }, 0);
+
+            Assert.IsTrue(started);
+            Assert.IsTrue(testPlayer.Playing);
+
+            testPlayer.Forward();
+
+            Assert.IsTrue(testPlayer.Playing);
+
+            testPlayer.Position = 19;
+            Thread.Sleep(1500);
+
+            Assert.IsTrue(testPlayer.Playing);
+
+            testPlayer.Position = 6;
+            testPlayer.Backward();
+
+            Assert.IsTrue(testPlayer.Playing);
+        }
+
+        [TestMethod]
+        public void ShuffleEverySongOnlyOnceAndRestart()
+        {
+            StartPlayer(out Player testPlayer, out List<MusicFile> fileList);
+
+            testPlayer.Shuffle = true;
+            testPlayer.Backward();
+
+            List<int> playedTracks = new List<int>();
+
+            for (int i = 0; i <= fileList.Count - 1; i++)
+            {
+                playedTracks.Add(testPlayer.PlayingFile.Track);
+                testPlayer.Forward();
+            }
+
+            for (int i = 0; i <= fileList.Count - 1; i++)
+            {
+                playedTracks.Remove(testPlayer.PlayingFile.Track);
+                testPlayer.Forward();
+            }
+
+            Assert.IsTrue(playedTracks.Count == 0);
+            Assert.IsTrue(testPlayer.Playing);
         }
     }
 }
