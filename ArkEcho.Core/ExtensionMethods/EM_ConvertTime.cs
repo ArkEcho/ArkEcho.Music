782b756bf4d58901424ba5c6ad16296d809111e0
Stephan Ziesenis <stephanz@gmx.de>
2023-01-28 17:04:56 +0100

    Anzeige Dauer, Album interpret; Convert Time

---
+++ ArkEcho.Core/ExtensionMethods/EM_ConvertTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArkEcho.Core
+{
+    public static class EM_ConvertTime
+    {
+        public static string ConvertTimeMilliseconds(this int value)
+        {
+            return ConvertTimeMilliseconds((long)value);
+        }
+
+        public static string ConvertTimeMilliseconds(this long value)
+        {
+            return TimeSpan.FromMilliseconds(value).ToString(@"mm\:ss");
+        }
+
+        public static string ConvertTimeSeconds(this int value)
+        {
+            return ConvertTimeSeconds((long)value);
+        }
+
+        public static string ConvertTimeSeconds(this long value)
+        {
+            return TimeSpan.FromSeconds(value).ToString(@"mm\:ss");
+        }
+    }
+}
